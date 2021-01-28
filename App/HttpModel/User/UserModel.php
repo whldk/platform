09c6a0eb0add2d0ff8c62973707c209aee9fd6cc
350553002@qq.com <350553002@qq.com>
2021-01-28 18:59:31 +0800

    modify

--- App/HttpModel/User/UserModel.php
+++ App/HttpModel/User/UserModel.php
@@ -2,7 +2,6 @@
 namespace App\HttpModel\User;
 
 use EasySwoole\ORM\AbstractModel;
-use Swoole\Table;
 
 class UserModel extends AbstractModel
 {
@@ -23,15 +22,4 @@ class UserModel extends AbstractModel
         'update_at' => 'timestamp'
     ];
 
-    public function schemaInfo(bool $isCache = true): Table
-    {
-        $table = new Table($this->tableName);
-        $table->colInt('id')->setIsPrimaryKey(true);
-        $table->colChar('username', 255);
-        $table->colChar('password', 255);
-        $table->colChar('real_name', 255);
-        $table->colInt('create_at');
-        $table->colInt('update_at');
-        return $table;
-    }
 }
